5e24cb88d4b156eb2b6fc95fb51e318e60383296
nottdark <nottdark.dev@gmail.com>
2025-08-27 00:23:25 +0700

    Setup project

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 4100;
+
+app.use(express.json());
+
+// CORS
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send({
+    ok: true,
+  });
+  console.log("GET [/]:", req.query);
+});
+
+app.get("/webhook", (req, res) => {
+  res.send({
+    success: true,
+  });
+  console.log("GET [/webhook]:", req.query);
+});
+
+app.post("/webhook", (req, res) => {
+  res.send({
+    success: true,
+  });
+  console.log("POST [/webhook]:", req.body);
+});
+
+app.listen(port, console.log(`Server is running on port ${port}`));
