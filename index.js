021bba61534ba9b6e8046a190a351da56ed98359
nottdark <nottdark.dev@gmail.com>
2025-08-27 00:57:52 +0700

    Add push message

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4100;
 
+// LINE SDK
+const lineBotSdk = require('@line/bot-sdk')
+// create LINE SDK config from env variables
+const config = {
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+};
+
+// middleware
+// lineBotSdk.middleware({
+//   channelSecret: config.channelSecret
+// });
+
+// create LINE SDK client
+const client = new lineBotSdk.messagingApi.MessagingApiClient({
+  channelAccessToken: config.channelAccessToken
+});
+
+
 app.use(express.json());
 
 // CORS
@@ -15,25 +34,50 @@ app.get("/version", (req, res) => {
   console.log("GET [/version]");
 });
 
+app.get("/config", (req, res) => {
+  console.log("GET [/config]", config);
+
+  res.send(config);
+});
+
 app.get("/", (req, res) => {
+  console.log("GET [/]:", req.query);
+
   res.send({
     ok: true,
   });
-  console.log("GET [/]:", req.query);
 });
 
 app.get("/webhook", (req, res) => {
+  console.log("GET [/webhook]:", req.query);
+
   res.send({
     success: true,
   });
-  console.log("GET [/webhook]:", req.query);
 });
 
 app.post("/webhook", (req, res) => {
+  console.log("POST [/webhook]:", req.body);
+
   res.send({
     success: true,
   });
-  console.log("POST [/webhook]:", req.body);
+});
+
+app.post("/send-message", async (req, res) => {
+  console.log("POST [/send-message]:", req.body);
+  
+  try {
+    await client.pushMessage({
+      to: req.body.userId,
+      messages: [{ type: 'text', text: req.body.text }]
+    })
+    res.send({
+      success: true,
+    });
+  } catch (error) {
+     res.status(400).send(error);
+  }
 });
 
 app.listen(port, console.log(`Server is running on port ${port}`));
