47dd540aa2c2f616e87f1c59f0025de85e9e6364
nottdark <nottdark.dev@gmail.com>
2025-08-27 12:54:24 +0700

    Add reply sticker

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const port = process.env.PORT || 4100;
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 4100
 
 // static
 app.use(express.static('public'))
@@ -12,113 +12,130 @@ const lineBotSdk = require('@line/bot-sdk')
 const config = {
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-};
+}
 
 // middleware
 lineBotSdk.middleware({
-  channelSecret: config.channelSecret
-});
+  channelSecret: config.channelSecret,
+})
 
 // create LINE SDK client
 const client = new lineBotSdk.messagingApi.MessagingApiClient({
-  channelAccessToken: config.channelAccessToken
-});
+  channelAccessToken: config.channelAccessToken,
+})
 
-
-app.use(express.json());
+app.use(express.json())
 
 // CORS
-app.use(cors());
+app.use(cors())
 
-app.get("/version", (req, res) => {
+app.get('/version', (req, res) => {
   res.send({
     version: '1.0.0',
-  });
-  console.log("GET [/version]");
-});
+  })
+  console.log('GET [/version]')
+})
 
-app.get("/config", (req, res) => {
-  console.log("GET [/config]", config);
+app.get('/config', (req, res) => {
+  console.log('GET [/config]', config)
 
-  res.send(config);
-});
+  res.send(config)
+})
 
-app.get("/", (req, res) => {
-  console.log("GET [/]:", req.query);
+app.get('/', (req, res) => {
+  console.log('GET [/]:', req.query)
 
   res.send({
     ok: true,
-  });
-});
+  })
+})
 
+app.post('/webhook', async (req, res) => {
+  console.log('POST [/webhook]:', req.body)
 
-app.post("/webhook", async (req, res) => {
-  console.log("POST [/webhook]:", req.body);
-
-  const event = req.body.events[0];
+  const event = req.body.events[0] // join, leave, message
 
   // Have Event
 
   try {
     if (event) {
-      console.log('[event.source object]:', JSON.stringify(event.source));
-  
+      console.log('[event.source object]:', JSON.stringify(event.source))
+
       if (event?.type === 'message') {
-        const message = event.message;
-        console.log('[message object]:', JSON.stringify(message));
-        
+        const message = event.message
+        console.log('[message object]:', JSON.stringify(message))
+
         if (message.type === 'text') {
           // BYE
           if (message.text === 'bye') {
             if (event.source.type === 'room') {
-              await client.leaveRoom(event.source.roomId);
+              await client.leaveRoom(event.source.roomId)
             } else if (event.source.type === 'group') {
-              await client.leaveGroup(event.source.groupId);
+              await client.leaveGroup(event.source.groupId)
             } else {
               client.replyMessage({
                 replyToken: event.replyToken,
-                messages: [{
-                  type: 'text',
-                  text: 'I cannot leave a 1-on-1 chat!',
-                }]
-              });
+                messages: [
+                  {
+                    type: 'text',
+                    text: 'I cannot leave a 1-on-1 chat!',
+                  },
+                ],
+              })
             }
+          } else if (['hi', 'hello', 'สวัสดี', 'ดี'].some((v) => v === text.toLocaleLowerCase())) {
+            await client.replyMessage({
+              replyToken: event.replyToken,
+              messages: [
+                {
+                  type: 'text',
+                  text: message.text + ' - Reply from BOT AI',
+                },
+                {
+                  type: 'sticker',
+                  packageId: '11537',
+                  stickerId: ['52002734', '52002738', '52002736'][Math.floor(Math.random() * 3)],
+                },
+              ],
+            })
           } else {
             await client.replyMessage({
               replyToken: event.replyToken,
-              messages: [{
-                type: 'text',
-                text: message.text + ' - Reply from BOT AI' ,
-              }]
-            });
-        }
+              messages: [
+                {
+                  type: 'text',
+                  text: message.text + ' - Reply from BOT AI',
+                },
+              ],
+            })
+          }
         }
       }
     }
   } catch (error) {
-    console.log('Error [webhook]:', error);
+    console.log('Error [webhook]:', error)
   }
 
   res.send({
-    success: true
-  });
-});
+    success: true,
+  })
+})
 
-app.post("/send-message", async (req, res) => {
-  console.log("POST [/send-message]:", req.body);
+app.post('/send-message', async (req, res) => {
+  console.log('POST [/send-message]:', req.body)
 
   try {
     await client.pushMessage({
       to: req.body.userId,
-      messages: [{ type: 'text', text: req.body.text }]
+      messages: [{ type: 'text', text: req.body.text }],
     })
     res.send({
       success: true,
-    });
+    })
   } catch (error) {
-    console.log('Error [/send-message]:', error);
-    res.status(400).send(error);
+    console.log('Error [/send-message]:', error)
+    res.status(400).send(error)
   }
-});
+})
 
-app.listen(port, console.log(`Server is running on port ${port}`));
+app.listen(port, console.log(`Server is running on port ${port}`))
