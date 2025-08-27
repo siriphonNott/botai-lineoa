2e6352186fab6828c88e947c086c51ff113e018b
nottdark <nottdark.dev@gmail.com>
2025-08-27 15:25:40 +0700

    Add setting Crypto-Prices

--- mock/db.js
+++ mock/db.js
@@ -6,5 +6,13 @@ export const channels = [
     group: 'crypto',
     channelSecret: '34c5347dbb562e09e1a1aec39e2bd285',
     channelAccessToken: 'UaJ3/dgD19Mf7ptI70ok12y8+sCAsHrgJjgf4MyCyxrrtv5nxB0AqsejC8H5I3v4VI5+kOHrCs3Y3RLGef4vIzcmVTo0XpzAYBk+v/Ws6Dyg1pQLMD3y+Edfei+vaau1OMsd0Zr6GOmfC+n6HKjXtQdB04t89/1O/w1cDnyilFU='
-  }
+  },
+  {
+    uid: 'oa00002',
+    channelName: 'Crypto-Prices',
+    baseID: '@986oiebr',
+    group: 'crypto',
+    channelSecret: '6111cfab14337713e7969fad86122ee4',
+    channelAccessToken: 'ffFsvjAlN1fWTvgfQD3uEBprXhRWnXLI4fyL+DoULTQzU5H3ZjI9ySQoP4r3BdV2sAMfwW0ysX2dbtPv0iQctmmFwclY3I1TkmRRv1CETO2jsnOubLleCrrIDKRZv6M+LtgFpXhNBLosPP71fghSmgdB04t89/1O/w1cDnyilFU='
+  },
 ]
